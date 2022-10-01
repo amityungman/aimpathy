5c44db19df80532138464c60e76ebb1aa9d6f502
amityungman <amityungman@gmail.com>
2022-10-01 16:47:41 +0200

    Has base of LSTM and CNN. Has a display for a wave form.

--- display_audio.py
+++ display_audio.py
@@ -1,28 +1,13 @@
+from aimpathy_constants import *
+from audio_utils import smooth
 import pyaudio
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from tkinter import TclError
-
-# constants
-CHUNK = 1024 * 2             # samples per frame
-FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
-CHANNELS = 1                 # single channel for microphone
-RATE = 44100                 # samples per second
-REFRESH_RATE = 0.03          # Rate of refresh of graphs in seconds
-SMOOTH_RATE = 4              # Rate of smoothing of the display graph
-GRAPH_SHOULDERS = 20
 
 
-def smooth(y, box_pts):
-    box = np.ones(box_pts)/box_pts
-    y_smooth = np.convolve(y, box, mode='same')
-    return y_smooth
-
-
-# create matplotlib figure and axes
-fig, (ax, ax2) = plt.subplots(2, figsize=(20, 7))
+"""********************   STARTING AUDIO STREAM   ********************"""
 
 # pyaudio class instance
 p = pyaudio.PyAudio()
@@ -37,37 +22,63 @@ stream = p.open(
     frames_per_buffer=CHUNK
 )
 
+
+"""********************   ADJUST DISPLAY WINDOW   ********************"""
+
+# create matplotlib figure and axes
+# fig, axes = plt.subplots(2, 2, figsize=(20, 7), gridspec_kw={'width_ratios': [3, 1]})
+# ((ax_wave, ax_specto), (ax_thayer, ax_emotion)) = axes
+fig, axes = plt.subplot_mosaic("AAB;CDD", figsize=(20, 7))
+ax_wave, ax_specto, ax_thayer, ax_emotion = axes.values()
+
+"""***   WAVEFORM   ***"""
+ax_wave.set_title('AUDIO WAVEFORM')
+ax_wave.set_xlabel('Samples')
+ax_wave.set_ylabel('Volume')
+ax_wave.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
+ax_wave.set_xlim(0, 2 * CHUNK)
+plt.setp(ax_wave, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
 # variable for plotting
-x = np.arange(0, 2 * CHUNK, 2)
+wave_x = np.arange(0, 2 * CHUNK, 2)
+wave_line, = ax_wave.plot(wave_x, np.random.rand(CHUNK), '-', lw=1)
 
-# create a line object with random data
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=1)
+"""***   SPECTROGRAM   ***"""
+ax_specto.set_title('SPECTROGRAM')
+ax_specto.set_xlabel('Time')
+ax_specto.set_ylabel('Hz')
+# ax_specto.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
+# ax_specto.set_xlim(0, 2 * CHUNK)
+# plt.setp(ax_specto, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
 
-# basic formatting for the axes
-ax.set_title('AUDIO WAVEFORM')
-ax.set_xlabel('samples')
-ax.set_ylabel('volume')
-ax.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
-ax.set_xlim(0, 2 * CHUNK)
-plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+"""***   THAYER MODEL   ***"""
+ax_thayer.set_title('Valance-Arousal graph (Thayer model)')
+ax_thayer.set_xlabel('Valance')
+ax_thayer.set_ylabel('Arousal')
+# ax_specto.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
+# ax_specto.set_xlim(0, 2 * CHUNK)
+# plt.setp(ax_specto, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
+"""***   EMOTIONS   ***"""
+ax_emotion.set_title('Perceived emotion graph')
+ax_emotion.set_xlabel('Time')
+ax_emotion.set_ylabel('Emotion level')
+ax_emotion.set_ylim(0 - GRAPH_SHOULDERS//5, 100 + GRAPH_SHOULDERS//5)
+# ax_specto.set_xlim(0, 2 * CHUNK)
+# plt.setp(ax_specto, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
 print("Display ready")
-
-# show the plot
 plt.show(block=False)
-print('stream started')
 
 while True:
-
     # binary data
     data = stream.read(CHUNK)
 
-    # convert data to integers, make np array, then offset it by 127
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-
-    # create np array and offset by 128
-    data_np = np.array([n for n in data_int], dtype='b')[::2] + 128
-    data_np = smooth(data_np, SMOOTH_RATE)
-    line.set_ydata(data_np)
+    # convert data to integers, for the wave form graph
+    wave_data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+    wave_data_np = np.array(wave_data_int, dtype='b')[::2] + 128
+    wave_data_np = smooth(wave_data_np, SMOOTH_RATE)
+    wave_line.set_ydata(wave_data_np)
 
     # update figure canvas
     try:
@@ -79,4 +90,4 @@ while True:
         print('stream stopped')
         break
 
-print('stream ended')
+print('ended')
