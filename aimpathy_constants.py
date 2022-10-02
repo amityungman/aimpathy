ff1e6fb102a705a6a46f924378b891d0ced8c341
amityungman <amityungman@gmail.com>
2022-10-02 21:49:07 +0200

    Added spectrogram [Tested]

--- aimpathy_constants.py
+++ aimpathy_constants.py
@@ -5,6 +5,15 @@ CHUNK = 1024 * 8             # samples per frame
 FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
 CHANNELS = 1                 # single channel for microphone
 RATE = 44100                 # samples per second
-REFRESH_RATE = 0.1           # Rate of refresh of graphs in seconds
+REFRESH_RATE = 0.01           # Rate of refresh of graphs in seconds
 SMOOTH_RATE = 4              # Rate of smoothing of the display graph
 GRAPH_SHOULDERS = 20         # The shoulders to buffer in the graph's display
+
+FFT_POINTS = CHUNK // 2
+FFT_POINTS_UNSIGNED = CHUNK // 4
+SECS_PER_SPECTROGRAM_SEGMENT = (FFT_POINTS - (FFT_POINTS // 8)) / RATE
+SPECTROGRAM_SECS = 5
+SPECTROGRAM_MIN_VALUE = 2 ** 11
+SPECTROGRAM_MAX_VALUE = 2 ** 15
+SPECTROGRAM_MIN_FREQUENCY = 100
+SPECTROGRAM_MAX_FREQUENCY = RATE // 2
--- display_audio.py
+++ display_audio.py
@@ -27,8 +27,8 @@ stream = p.open(
 
 """********************   ADJUST DISPLAY WINDOW   ********************"""
 
-fig, axes = plt.subplot_mosaic("AAB;CDD", figsize=(20, 7))
-ax_wave, ax_specto, ax_thayer, ax_emotion = axes.values()
+fig, axes = plt.subplot_mosaic("ABC;DDC", figsize=(20, 7))
+ax_wave, ax_thayer, ax_spectro, ax_emotion = axes.values()
 
 """***   WAVEFORM   ***"""
 ax_wave.set_title('Audioform')
@@ -43,38 +43,39 @@ wave_line, = ax_wave.plot(wave_x, np.random.rand(CHUNK), '-', lw=1)
 
 
 """***   SPECTROGRAM   ***"""
-ax_specto.set_title('Spectrogram')
-ax_specto.set_xlabel('Time [sec]')
-ax_specto.set_ylabel('Frequency [Hz]')
-# specto_y, specto_x = np.meshgrid(np.linspace(0, RATE, RATE//10),
-#                                  np.linspace(0, SPECTROGRAM_SECS * 60, int((SPECTROGRAM_SECS * 60) / REFRESH_RATE)))
-# specto_z = np.zeros(specto_x.shape)
-# specto_z = specto_z[:-1, :-1]
-# specto_z_min, specto_z_max = 0, 256
-#
-# ax_specto.set_yscale('log')
-# ax_specto.set_ylim(1, RATE)
-# ax_specto.set_xlim(0, SPECTROGRAM_SECS * 60)
-# plt.setp(ax_specto, xticks=np.linspace(0, SPECTROGRAM_SECS * 60, (SPECTROGRAM_SECS * 60 // 15) + 1),
-#          yticks=[10**i for i in range(0, math.ceil(math.log(RATE, 10)))])
-# specto_heatmap = ax_specto.pcolormesh(specto_x, specto_y, specto_z, cmap='viridis', vmin=specto_z_min, vmax=specto_z_max)
-# fig.colorbar(specto_heatmap, ax=ax_specto)
+ax_spectro.set_title('Spectrogram')
+ax_spectro.set_xlabel('Time [sec]')
+ax_spectro.set_ylabel('Frequency [Hz]')
+spectro_x_size = SPECTROGRAM_SECS * math.ceil(1 / SECS_PER_SPECTROGRAM_SEGMENT)  # num of spectrograms to display
+spectro_y_size = FFT_POINTS_UNSIGNED + 1  # num of frequencies
+spectro_x = np.array([(FFT_POINTS_UNSIGNED / RATE) + i * SECS_PER_SPECTROGRAM_SEGMENT for i in range(0, spectro_x_size)])
+spectro_y = np.linspace(0, RATE // 2, spectro_y_size)
+spectro_z = np.zeros((spectro_y.shape[0], spectro_x.shape[0]))
+spectro_z_min, spectro_z_max = SPECTROGRAM_MIN_VALUE, SPECTROGRAM_MAX_VALUE
+
+ax_spectro.set_yscale('log')
+ax_spectro.set_ylim(SPECTROGRAM_MIN_FREQUENCY, SPECTROGRAM_MAX_FREQUENCY)
+ax_spectro.set_xlim(0, SPECTROGRAM_SECS)
+plt.setp(ax_spectro, yticks=[10 ** i for i in range(0, math.ceil(math.log(RATE, 10)))])
+spectro_heatmap = ax_spectro.pcolormesh(spectro_x, spectro_y, spectro_z, cmap='magma', vmin=spectro_z_min,
+                                        vmax=spectro_z_max)
+# fig.colorbar(spectro_heatmap, ax=ax_spectro)
 
 """***   THAYER MODEL   ***"""
 ax_thayer.set_title('Valance-Arousal graph (Thayer model)')
 ax_thayer.set_xlabel('Valance')
 ax_thayer.set_ylabel('Arousal')
-# ax_specto.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
-# ax_specto.set_xlim(0, 2 * CHUNK)
-# plt.setp(ax_specto, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+# ax_spectro.set_ylim(0 - GRAPH_SHOULDERS, 255 + GRAPH_SHOULDERS)
+# ax_spectro.set_xlim(0, 2 * CHUNK)
+# plt.setp(ax_spectro, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
 
 """***   EMOTIONS   ***"""
 ax_emotion.set_title('Perceived emotion graph')
 ax_emotion.set_xlabel('Time')
 ax_emotion.set_ylabel('Emotion level')
 ax_emotion.set_ylim(0 - GRAPH_SHOULDERS//5, 100 + GRAPH_SHOULDERS//5)
-# ax_specto.set_xlim(0, 2 * CHUNK)
-# plt.setp(ax_specto, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+# ax_spectro.set_xlim(0, 2 * CHUNK)
+# plt.setp(ax_spectro, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
 
 print("Display ready")
 plt.show(block=False)
@@ -91,15 +92,10 @@ while True:
     count = len(data) / 2
     format = '%dh' % (count)
     snd_block = np.array(struct.unpack(format, data))
-    f, t, Sxx = signal.spectrogram(snd_block, RATE, scaling="spectrum", nperseg=CHUNK//2)
-    specto_heatmap = ax_specto.pcolormesh(t, f, Sxx, cmap='viridis')
-    plt.setp(ax_specto, yticks=[10**i for i in range(0, math.ceil(math.log(RATE, 10)))])
-    ax_specto.set_yscale('log')
-    ax_specto.set_ylim(20, RATE//3)
-    # specto_data = np.fft.fft(data_int)
-    # specto_z = np.roll(specto_z, 1, axis=0)
-    # specto_z[0] = specto_data
-    # specto_heatmap.set_zdata(specto_z)
+    f, t, Sxx = signal.spectrogram(snd_block, RATE, scaling="spectrum", nperseg=FFT_POINTS)
+    spectro_z = np.roll(spectro_z, 2, axis=1)
+    spectro_z[:, 0:2] = Sxx
+    spectro_heatmap.set_array(spectro_z)
 
     # update figure canvas
     try:
